486f6b83e366a590627b079d181227998859d790
unknown <jeremiahopara4@gmail.com>
2022-07-12 22:03:19 +0100

    3rd commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed("".typing",{
+    strings:['Web Designer', 'Web Developer', 'Web App Creator', 'Writer'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
